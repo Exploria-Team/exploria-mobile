2cfacfed8043a7b2f6756aeb4d3c5096fdbfc4b6
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2024-11-28 23:11:17 +0700

    Update UserPreference.kt to use Hilt and added to Module

---
+++ app/src/main/java/com/app/exploria/di/Module.kt
@@ -0,0 +1,50 @@
+package com.app.exploria.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.room.Room
+import com.app.exploria.data.database.ItinerariesDatabase
+import com.app.exploria.data.database.dao.ItinerariesDao
+import com.app.exploria.data.pref.UserPreference
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context): ItinerariesDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            ItinerariesDatabase::class.java,
+            "itineraries_database"
+        ).build()
+    }
+
+    @Provides
+    fun provideItinerariesDao(database: ItinerariesDatabase): ItinerariesDao {
+        return database.itinerariesDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.dataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserPreferences(database: DataStore<Preferences>): UserPreference {
+        return UserPreference(database)
+    }
+}
